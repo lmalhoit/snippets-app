9aeaf7feb7b1a46351641f5503b5e1660039c49a
Lauren Malhoit <laurenmalhoit@gmail.com>
2015-03-07 20:18:15 +0000

    new commits

--- snippets.py
+++ snippets.py
@@ -10,21 +10,29 @@ logging.debug("Database connection established.")
 def put(name, snippet):
     """Store a snippet with an associated name."""
     logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
-    cursor = connection.cursor()
-    command = "insert into snippets values (%s, %s)"
-    cursor.execute(command, (name, snippet))
-    connection.commit()
+    with connection, connection.cursor() as cursor:
+      try:
+        command = "insert into snippets values (%s, %s)"
+        cursor.execute(command, (name, snippet))
+      except psycopg2.IntegrityError as e:
+        connection.rollback()
+        command = "update snippets set message=%s where keyword=%s"
+        cursor.execute(command, (snippet, name))
     logging.debug("Snippet stored successfully.")
     return name, snippet
   
 def get(name):
     """Retrieve the snippet with a given name."""
     logging.info("Retrieving a snippet {!r}".format(name))
-    cursor = connection.cursor()
-    cursor.execute("select message from snippets where keyword={!r}".format(name))    
-    row = cursor.fetchone()
+    with connection, connection.cursor() as cursor:
+      cursor.execute("select message from snippets where keyword=%s", (name,))
+      row = cursor.fetchone()
     logging.debug("Snippet retrieved successfully.")
-    return row[0]
+    if not row:
+        # No snippet was found with that name.
+      print "There's no snippet with that name"
+    else:
+      return row[0]
     
 def main():
     """Main function"""
